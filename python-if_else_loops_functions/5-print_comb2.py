7495a77f69d2e3b335b92ad2f5489517402b95c2
Haidar Alessa <10682@holbertonstudents.com>
2025-05-11 10:26:19 +0000

    print numbers from 00 to 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in range(100):
-    print(f"0{i}" if i < 10 else f"{i}", end=", ")
-print("")
+numbers = [f"0{i}" if i < 10 else f"{i}" for i in range(100)]
+print(*numbers, sep=", ")
