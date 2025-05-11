32157c570af6d39fa303ffd6ef52120ba5b04cf6
Haidar Alessa <10682@holbertonstudents.com>
2025-05-11 17:50:17 +0000

    uppsercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    return chr(ord(str)-32)
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            u = chr(ord(str) - 32)
+        print("{}".format(u), end="")
+    print()
