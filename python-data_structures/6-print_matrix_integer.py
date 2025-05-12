2669ef01c6e73ded271f6ee9d5e6a453c88dcf76
Haidar Alessa <10682@holbertonstudents.com>
2025-05-12 09:47:54 +0000

    remove extra space

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for e in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][e]), end=" ")
+            print("{:d}".format(matrix[i][e]), end="")
+            if e < len(matrix[i]) - 1:
+                print(" ", end="")
         print()
