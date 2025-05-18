3e4dc69f7e40bb234151ae7c2c46e7984d03d762
Haidar Alessa <10682@holbertonstudents.com>
2025-05-18 18:57:16 +0000

    safe print list:

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
+    printedItems = 0
+
+    for i in range(x):
+        try:
             print(f"{my_list[i]}", end="")
-        print()
-    except:
-        print()
+            printedItems += 1
+        except IndexError:
+            break
+    print()
+    return printedItems
