ee95927cbdb6d5da27050f5e00fdc5cdcf6c46fd
Haidar Alessa <10682@holbertonstudents.com>
2025-05-18 19:12:42 +0000

    safe print integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value / value == 1:
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except BaseException:
         return False
