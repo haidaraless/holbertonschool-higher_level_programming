9cdd1e92f82df7ed2ae9afbfec7fb19c164a8fe5
Haidar Alessa <10682@holbertonstudents.com>
2025-05-21 06:40:01 +0000

    Square with validating size input

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""declare a private class attribute (variable) to return size"""
+
+
+class Square:
+    """a class to represent Square"""
+    __size = 0
+
+    def __init__(self, size=0):
+        """an instance variable to accept the size with validating input"""
+
+        try:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+
+            self.__size = size
+        except TypeError:
+            raise TypeError("size must be an integer")
