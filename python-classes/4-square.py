b13ac401fb7f829aebd0cd77e84e454994bfbe91
Haidar Alessa <10682@holbertonstudents.com>
2025-05-21 07:52:30 +0000

    getter and setter for square size

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""declare a private class attribute (variable) to return size"""
+
+
+class Square:
+    """a class to represent Square"""
+    __size = 0
+
+    def __init__(self, size=0):
+        """an instance variable to accept the size with validating input"""
+
+        self.__size = size
+
+    def area(self):
+        """a public method that calculates area of square"""
+        return self.__size**2
+
+    @property
+    def size(self):
+        """a property to get size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """a property to set size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
