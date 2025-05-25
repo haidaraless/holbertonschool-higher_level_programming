25a6210ce121296730beb0f94d305c41ed7c8519
Haidar Alessa <10682@holbertonstudents.com>
2025-05-25 13:22:19 +0000

    update logic code

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -27,15 +27,17 @@ class Rectangle:
         """a setter of width attribute"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @height.setter
     def height(self, value):
         """a setter of height attribute"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
