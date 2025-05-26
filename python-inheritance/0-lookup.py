163ffe80d7ac812573c7e28eca7fad301b319bf8
Haidar Alessa <10682@holbertonstudents.com>
2025-05-26 16:38:43 +0000

    Add comments to module and function

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+lookup for available attributes and methods of an object
+"""
+
 
 def lookup(obj):
+    """
+    return a list of all available attributes and methods of the given object
+    """
     return dir(obj)
