e36cc7d5677f3061aa4b8fca9c7e52985bca3f6b
Haidar Alessa <10682@holbertonstudents.com>
2025-06-01 17:03:23 +0000

    calculates Rectangle area

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+A class that represents Rectangle which is inherits BaseGeometry class
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+
+    """
+    Declaring a Rectangle class
+    """
+
+    def __init__(self, width, height):
+        """
+        Accepts two arguments (width & height) to represent Rectangle
+        """
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        calculates rectangle area
+        """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        prints representation of Rectangle class
+        """
+        return "[{}] {}/{}".format(
+            self.__class__.__name__, self.__width, self.__height)
