60b1d07d427b4308bf272d56bec6b2a61e091342
Haidar Alessa <10682@holbertonstudents.com>
2025-06-01 13:32:01 +0000

    inherits from

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-Checks if an object's class is inherits from a given class directly or indirectly
+Checks if an object's class is inherits from a given class
+directly or indirectly
 """
 
 
 def inherits_from(obj, a_class):
     """
     Return True if object's class is inherits from a_class
-    directly or indirectly otherwise returns False 
+    directly or indirectly otherwise returns False
     """
 
-    return issubclass(type(obj), a_class)
+    return not type(obj) is a_class and issubclass(type(obj), a_class)
