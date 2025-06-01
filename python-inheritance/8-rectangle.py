9c124b5f121aa8073aa95927db1fb060e8984ab1
Haidar Alessa <10682@holbertonstudents.com>
2025-06-01 14:28:32 +0000

    Add some files

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""
+A class that represents Rectangle which is inherits BaseGeometry class
+"""
+
+
+class Rectangle(BaseGeometry):
+
+    """
+    Declaring a Rectangle class
+    """
+
+    def __init__(self, width, height):
+        """
+        Accepts two arguments (width & height) to represent Rectangle
+        """
+    
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
