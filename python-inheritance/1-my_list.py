847086d14f3c28915df293648b77c43e5fb32f50
Haidar Alessa <10682@holbertonstudents.com>
2025-06-01 12:07:42 +0000

    Documenting the module and it methods

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+Inherit MyList from the original (built-in) list
+"""
+
+
 class MyList(list):
 
+    """
+    prints sorted list (ascending sort)
+    """
     def print_sorted(self):
         print(sorted(self))
