72f2096aa982d19e6da861099e398f8458df59b1
Haidar Alessa <10682@holbertonstudents.com>
2025-06-14 16:48:22 +0000

    Module and function documentations

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Declaring a function that reads a file and print its content
+"""
+
 
 def read_file(filename=""):
+    """
+    reads file's content and print it out
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
