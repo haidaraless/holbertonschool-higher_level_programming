c4a4594b53d782df4104f1688bf3b769ffc2762b
Haidar Alessa <10682@holbertonstudents.com>
2025-06-14 16:59:17 +0000

    writes text into file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+writes a string into file content
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    writes a text into file
+    """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        f.write(text)
+    return len(text)
