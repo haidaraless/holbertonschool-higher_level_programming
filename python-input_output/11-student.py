44f233edd21c14033a6108eb2f78c404bf2940ff
Haidar Alessa <10682@holbertonstudents.com>
2025-06-15 06:26:58 +0000

    replace all attributes

---
+++ python-input_output/11-student.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""
+A class that represents Student
+"""
+
+
+class Student:
+    """
+    Define Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor of Student class that accepts
+        first and last name, and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation
+        """
+        if attrs is None:
+            return self.__dict__
+        result = {}
+
+        for attr_name in attrs:
+            if attr_name == "age":
+                result["age"] = self.age
+            elif attr_name == "first_name":
+                result["first_name"] = self.first_name
+            elif attr_name == "last_name":
+                result["last_name"] = self.last_name
+        return result
+
+    def reload_from_json(self, json):
+        """
+        replace all attributes with the attributes in json
+        """
+        for key, value in json.items():
+            if key == "first_name":
+                self.first_name = value
+            elif key == "last_name":
+                self.last_name = value
+            elif key == "age":
+                self.age = value
