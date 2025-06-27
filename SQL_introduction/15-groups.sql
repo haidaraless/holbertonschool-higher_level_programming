bdf9dbb81ef2c16f760f26ccdc0fb994b899a9d0
Haidar Alessa <10682@holbertonstudents.com>
2025-06-27 11:25:48 +0000

    list the number of records with the same score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Group the result data by a given column
-SELECT `score`, COUNT(`id`) AS number FROM second_table GROUP BY score;
+SELECT `score`, COUNT(`id`) AS number FROM second_table GROUP BY score ORDER BY number DESC;
