69bbca8e4ff30854b1b42aa635e4d89f9d1fc388
Haidar Alessa <10682@holbertonstudents.com>
2025-06-27 14:14:23 +0000

    creates a user to MySQL Server and grant all privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates a user to MySQL Server
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
