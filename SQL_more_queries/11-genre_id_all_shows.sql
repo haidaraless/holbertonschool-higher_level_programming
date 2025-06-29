6ab98c1193684c3ff0a87623d354479970d51565
Haidar Alessa <10682@holbertonstudents.com>
2025-06-29 07:16:49 +0000

    list all tv shows, null genre included

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- List all tv shows with null genre included
 
-SELECT tv_shows.title, IFNULL(tv_shows_genres.genre_id, NULL)
+SELECT tv_shows.title, tv_shows_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
