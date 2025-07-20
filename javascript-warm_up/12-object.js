312e27e330968f093e723f23e5b88fac392f57f1
Haidar Alessa <10682@holbertonstudents.com>
2025-07-20 18:15:14 +0000

    fix styling

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-const obj = {
-    type: 'object',
-    value: 12
-  };
-  console.log(obj);
-  obj.value = 89;
-  console.log(obj);
+const obj = { type: 'object', value: 12};
+console.log(obj);
+obj.value = 89;
+console.log(obj);
