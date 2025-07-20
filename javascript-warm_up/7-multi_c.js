8f4cb0e8716b81eedd0766bb20c7944f1deee557
Haidar Alessa <10682@holbertonstudents.com>
2025-07-20 17:26:30 +0000

    print n times

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const times = process.argv[2];
+
+if(! parseInt(times)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(times); i++) {
+    console.log('C is fun');
+  }
+}
