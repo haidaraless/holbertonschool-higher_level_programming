71906b6b0373bc29b19466e9e843b620ece11968
Haidar Alessa <10682@holbertonstudents.com>
2025-07-20 18:04:39 +0000

    factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function factorial (n) {
+    if (!parseInt(n)) {
+        return 1;
+    }
+    if (n === 0) {
+        return 1;
+    }
+    return factorial(n - 1) * n;
+}
+console.log(factorial(process.argv[2]));
