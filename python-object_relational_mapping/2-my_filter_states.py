76400a3025bd88407bf93010b6e46e51b208f23d
Haidar Alessa <10682@holbertonstudents.com>
2025-07-31 06:31:34 +0000

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Script to lists all states where name takes from user
+from the database hbtn_0e_0_usa
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         passwd=password, db=db_name, charset="utf8")
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+        id ASC").format(sys.argv[4])
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+
+    cursor.close()
+    db.close()
