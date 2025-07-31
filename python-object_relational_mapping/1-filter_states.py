e6f4bb0d3a32f01f4f215089c599d3cb69260d5e
Haidar Alessa <10682@holbertonstudents.com>
2025-07-31 06:23:12 +0000

    tak 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script to lists all states from the database hbtn_0e_0_usa
+Usage: ./0-select_states.py <username> <password> <database>
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         passwd=password, db=db_name, charset="utf8")
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
