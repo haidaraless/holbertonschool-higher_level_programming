371613c3b02d04bdc1c62fe39579bb78142448e0
Haidar Alessa <10682@holbertonstudents.com>
2025-07-31 10:30:15 +0000

    task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -20,20 +20,20 @@ def validate_inputs(matrix, div):
     validates matrix and division number
     """
 
-    if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix\
-        (list of lists) of integers/floats")
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
 
-    for r in range(len(matrix)):
-        if not isinstance(matrix[r], list):
-            raise TypeError("matrix must be a matrix\
-                (list of lists) of integers/floats")
-
-    if len(matrix[0]) != len(matrix[1]):
+    if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    if not isinstance(div, int):
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
